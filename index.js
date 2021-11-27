c581d74fed5ebfe80dbf978b81ab38c05bddce07
febbynf <febbynurfitriyani1208@gmail.com>
2021-11-27 11:35:02 +0700

    add planet button

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 
 let scene = document.getElementById("scene");
 let parallaxInstance = new Parallax(scene);
+
+//swiper
+
+let keys = [
+    "Mercury",
+    "Venus"
+];
+
+let slider = new Swiper(".swiper-container", {
+    slidesPreView: "auto",
+    spaceBetween: 150,
+    centeredSlides: true,
+    mousewheel: true,
+    pagination: {
+        el: ".planet-links",
+        clickable: true,
+        renderBullet: function(index, className) {
+            return '<div class="' + className + '">' + keys[index] + "</div>";
+        }
+    }
+});
